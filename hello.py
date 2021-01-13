72f52d8d6e44ad005faedad18889de4925d3583e
Aliflo <tom@birkbeck.net>
2021-01-13 11:33:26 +0000

    final commit

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 name = 'Yi'
 age = 18
 birthday = "11th Nov"
-next_year = age + 1
 
-print(f'Hi, my name is {name}, and I am {age} years old. My birthday is {birthday}')
+print(f'Hi, my name is {name}, and I am {age} years old.')
+print(f"On my birthday, the {birthday} I will be {age+1}.")
