c8e6b36596d0a7495568bf97ef06f5cc72dc8b7c
Yi Liu <s1901963@ed.ac.uk>
2021-01-13 19:30:57 +0800

    Yi

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-name = 'Tom'
+name = 'Yi'
 age = 18
-birthday = "11th August"
+birthday = "11th Nov"
+next_year = age + 1
 
 print(f'Hi, my name is {name}, and I am {age} years old. My birthday is {birthday}')
