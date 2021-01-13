42fb77f1ae4867cb099e2556fbac8ce2685e10bf
Aliflo <tom@birkbeck.net>
2021-01-13 11:16:45 +0000

    second commit from tom

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 name = 'Tom'
 age = 18
+birthday = "11th August"
 
-print(f'Hi, my name is {name}, and I am {age} years old.')
+print(f'Hi, my name is {name}, and I am {age} years old. My birthday is {birthday}')
